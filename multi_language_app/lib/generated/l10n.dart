4904cd929ba9b0b5ee28741f3cc9afd96726dc33
diepdn <diepdn@datascience.com.vn>
2020-10-05 14:44:44 +0700

    Intl l10n

---
+++ multi_language_app/lib/generated/l10n.dart
@@ -0,0 +1,94 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+
+class S {
+  S();
+  
+  static S current;
+  
+  static const AppLocalizationDelegate delegate =
+    AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name); 
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      S.current = S();
+      
+      return S.current;
+    });
+  } 
+
+  static S of(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Hello everyone`
+  String get hello {
+    return Intl.message(
+      'Hello everyone',
+      name: 'hello',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Goodbye to you`
+  String get goodbye {
+    return Intl.message(
+      'Goodbye to you',
+      name: 'goodbye',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `You have pushed the button this many times:`
+  String get button_intro {
+    return Intl.message(
+      'You have pushed the button this many times:',
+      name: 'button_intro',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'jp'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    if (locale != null) {
+      for (var supportedLocale in supportedLocales) {
+        if (supportedLocale.languageCode == locale.languageCode) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
